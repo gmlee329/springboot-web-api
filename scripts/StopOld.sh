c938aee2c03a83c24a5e69d09b7db790afd8a7c4
gmlee329 <gmlee329@naver.com>
2021-08-19 20:46:48 +0900

    modify StopOld.sh

--- scripts/StopOld.sh
+++ scripts/StopOld.sh
@@ -8,7 +8,7 @@ CURRENT_PID=$(ps -e | grep java | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  ehco "> java application is not running"
+  echo "> java application is not running"
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
